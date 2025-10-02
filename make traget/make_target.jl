4d3080a50cc5946a6464f07a76677b4e7448d8b5
Yakir Gagnon <12.yakir@gmail.com>
2025-10-02 17:09:55 +0200

    kaka

--- make traget/make_target.jl
+++ make traget/make_target.jl
@@ -9,23 +9,27 @@ using CSV
 inch = 96
 pt = 4/3
 mm = inch / 2.54 / 10
-size = (210mm, 297mm)
+sz = (210mm, 297mm)
+
+radius = 0.8min(sz...)/2
 
 buff = 0.15
 
 square = Point2f[(0,0), (1, 0), (1, 1), (0, 1), (0, 0)]
 
-cs = [(Point2f(x, y) .+ randn(Point2f)/50) .* size for x in (buff, 1 - buff) for y in (buff, 1 - buff)]
+cs = [(Point2f(x, y) .+ randn(Point2f)/50) .* sz for x in (buff, 1 - buff) for y in (buff, 1 - buff)]
 θs = 2π*rand(4)
 
 
-fig = Figure(; size, figure_padding = 0)
-ax = Axis(fig[1,1], limits = (0, size[1], 0, size[2]), aspect = DataAspect())#, backgroundcolor = :gray80)
+fig = Figure(; size = sz, figure_padding = 0)
+ax = Axis(fig[1,1], limits = (0, sz[1], 0, sz[2]), aspect = DataAspect())#, backgroundcolor = :gray80)
 hidespines!(ax)
 hidedecorations!(ax)
-x = range(0.2size[1], 0.8size[1], step = 10mm)
-y = range(0.2size[2], 0.8size[2], step = 10mm)'
-scatter!(ax, vec(Point2f.(x, y)), color = :black)
+x = range(0, sz[1], step = 10mm)
+y = range(0, sz[2], step = 10mm)'
+xy = vec(Point2f.(x, y))
+filter!(p -> norm(p - Point2f(sz ./2)) < radius, xy)
+scatter!(ax, xy, color = :black)
 square_width = 25mm
 for k in 1:4
     at = getAprilTagImage(k)
@@ -34,7 +38,8 @@ for k in 1:4
         poly!(ax, trans.(square), color = at[i, j], strokewidth = 0)
     end
 end
-text!(ax, [c + Point2f(0, 0.75square_width) for c in cs], text = string.([round.(Int, c) for c in cs], " at ", round.(Int, rad2deg.(θs)), "°"), align = (:center, :center))
+# text!(ax, [c + Point2f(0, 0.75square_width) for c in cs], text = string.([round.(Int, c) for c in cs], " at ", round.(Int, rad2deg.(θs)), "°"), align = (:center, :center))
+lines!(ax, Circle(Point(sz ./ 2), radius), color = :black)
 
 save("toprint.pdf", fig)
 CSV.write("data.csv", [(; x = first(c), y = last(c), θ) for (c, θ) in zip(cs, θs)])
